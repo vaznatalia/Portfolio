48fb32f38b27e6f968d355b61e278c5486147453
vaznatalia <natalialuizavaz@gmail.com>
2019-12-31 17:54:10 -0500

    intro page with box

--- src/Introduction.js
+++ src/Introduction.js
@@ -3,13 +3,44 @@ import Particles from 'react-particles-js';
 import { Link } from 'react-router-dom';
 
 class Example extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isHovered: false };
+
+  }
+
+  handleEnter() {
+    this.setState({
+      isHovered: true 
+    });
+  }
+  
+  handleLeave() {
+    this.setState({
+      isHovered: false 
+    });
+  }
   render () {
     return (
       <>
      
        <p className="animatedLetter"> ESCAPE INTO AMAZING EXPERIENCES</p>
-      <i  class="fas fa-angle-double-down"></i>
-      
+      <i onMouseEnter={this.handleEnter.bind(this)}
+            onMouseLeave={this.handleLeave.bind(this)}  class="fas fa-angle-double-down"></i>
+       {this.state.isHovered ? (
+          <a class="btn boxx">
+            <p>
+              I'm a creative Full Stack Web Developer <br/>
+              who loves animation and work with React 
+            </p>
+          </a>     
+               ) : (
+            <div />
+          )}
+        
         <Particles style={{zIndex:'-1'}} />
        
       
